3def04609620d03a6c4158bfb66a69c216867d7b
Павел <Pavel-grudev@yandex.ru>
2023-05-02 20:23:12 +0300

    Initial commit

---
+++ src/main/java/pro/sky/testmav2/Testmav2Application.java
@@ -0,0 +1,12 @@
+package pro.sky.testmav2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Testmav2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testmav2Application.class, args);
+	}
+
+}
